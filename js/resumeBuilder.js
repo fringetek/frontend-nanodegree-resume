42c2456b1ae637012ac3e0b18a667f6c744bbbe4
hooahvixen <rachel@shebora.com>
2015-03-19 14:23:48 -0400

    Make header look pretty w/js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,12 @@
-$("#main").append("Rachel Presley")
+var myName =
+  "Rachel Presley";
+var formattedName =
+ HTMLheaderName.replace("%data%", myName);
+
+var role =
+  "Front-end Web Developer";
+var formattedRole =
+ HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedRole);
+$("#header").prepend(formattedName);
