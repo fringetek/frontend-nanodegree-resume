aea4f3a3f12d959eb6dc62952f23a550c3217f80
hooahvixen <rachel@shebora.com>
2015-03-20 15:39:15 -0400

    only JSON objects, change empty display to black

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,75 +1,103 @@
-/*Header*/
-var myName =
-  "Rachel Presley";
-var formattedName =
- HTMLheaderName.replace("%data%", myName);
-
-var role =
-  "Front-end Web Developer";
-var formattedRole =
- HTMLheaderRole.replace("%data%", role);
-
-$("#header").append(formattedRole);
-$("#header").prepend(formattedName);
-
 /*bio*/
-var skills =
- ["HTML/CSS", "JavaScript", "JQuery", "PHP"];
-
 var bio = {
-  "name" : "Rachel",
+  "name" : "Rachel Presley",
   "age" : 26,
-  "role" : role,
+  "role" : "Front-end Web Developer",
   "phone" : "(703) 647 - 0180",
   "email" : "rachel@shebora.com",
   "picURL" : "https://scontent-atl.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11011889_10206303855420310_1162186412875955130_n.jpg?oh=84b000034ae31227c24b6313ec6d7a42&oe=5584B7D3",
   "welcome" : "Long days and pleasant nights, humans.",
-  "skills" : skills,
+  "skills" : ["HTML/CSS", "JavaScript", "JQuery", "PHP"],
   "git" : "HooahVixen",
   "loc" : "North Carolina"
 }
 
-var formattedPic = HTMLbioPic.replace("%data%", bio.picURL);
-$("#header").prepend(formattedPic);
+/*Work*/
+var work = [
+  {
+    "position" : "Domestic Engineer",
+    "employer" : "Unwanted Children",
+    "years" : 7,
+    "city" : "Worldwide"
+  },
+  {
+    "position" : "Pregnant Receptionist",
+    "employer" : "Dance Factory",
+    "years" : 2008,
+    "city" : "Arlington, VA"
+  },
+  {
+    "position" : ["Intern", "Receptionist", "Dance Instructor", "Executive Assistant"],
+    "employer" : "Alliance Dance Institute",
+    "years" : "2005-2007",
+    "city" : "Alexandria, VA"
+  }
 
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
-$("#header").append(formattedWelcome);
+]
 
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-$("#topContacts").append(formattedEmail);
-
-var formattedPhone = HTMLmobile.replace("%data%", bio.phone);
-$("#topContacts").append(formattedPhone);
-
-var formattedGit = HTMLgithub.replace("%data%", bio.git);
-$("#topContacts").append(formattedGit);
-
-var formattedLoc = HTMLlocation.replace("%data%", bio.loc);
-$("#topContacts").append(formattedLoc);
-
-var formattedSkills = HTMLskills.replace("%data%", bio.skills.join("</li><li>"))
-$("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills);
-
-
-/*Work/edu*/
-var work = {
-  "position" : "Domestic Engineer",
-  "employer" : "Unwanted Children",
-  "years" : 7,
-  "city" : "Worldwide"
-};
-
+/*education*/
 var education = {
-  "name" : "GED",
-  "years" : "0",
-  "city" : "Fairfax, VA"
+    "highSchools": [
+        {
+            "name": "Park View High School",
+            "city": "Sterling, VA",
+            "years": "2002-2003",
+            "grad": false
+        },
+        {
+            "name": "Robinson Secondary School",
+            "city": "Burke, VA",
+            "years": ["2003-2005", "2007"],
+            "grad": false
+        },
+        {
+            "name": "T. C. Williams",
+            "city": "Alexandria, VA",
+            "years": "2005-2006",
+            "grad": false
+        },
+        {
+            "name": "GED",
+            "city": "Fairfax, VA",
+            "years": "2007",
+            "grad": true
+        }
+    ],
+    "colleges": [
+        {
+            "name": "University of Pheonix",
+            "city": "Fort Polk, LA",
+            "major": "Elementary Education",
+            "grad": [2009, false],
+            "online": true
+        }
+    ],
+    "onlineCourses" : [
+      {
+        "schoolName" : "Khan Academy",
+        "siteURL" : "https://www.khanacademy.org/",
+        "userName" : "@hooahvixen",
+        "compCourses" : ["Intro to JS: Drawing & Animation", "HTML/CSS: Making webpages", "Hour of Code", "K-2nd Math", "Early Math Fundamentals"],
+        "enrolledIn" : ["Advanced JS: Games & Visualizations", "Trigonometry", "Pre-Calculus", "Chemistry"],
+        "years" : "2014 - Present"
+      },
+      {
+        "schoolName" : "Code Academy",
+        "siteURL" : "http://www.codecademy.com",
+        "userName" : "hooahvixen",
+        "compCourses" : ["JavaScript", "jQuery"],
+        "enrolledIn" : "PHP",
+        "years" : 2015
+      },
+      {
+        "schoolName" : "Udacity",
+        "siteURL" : "https://www.udacity.com/",
+        "userName" : "rachel@shebora.com",
+        "compCourses" : ["Intro to HTML and CSS", "How to Use Git and GitHub"],
+        "enrolledIn" : ["JavaScript Basics", "Responsive Web Design Fundamentals"],
+        "years" : 2015
+      }
+    ]
 }
-
-var workPosition = HTMLworkTitle.replace("%data%", work["position"]);
-$("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(workPosition);
-
-$("#education").append(HTMLschoolStart);
-var schoolName = HTMLschoolName.replace("%data%", education.name);
-$("#education").append(schoolName);
+/*Projects*/
+var projects = {};
