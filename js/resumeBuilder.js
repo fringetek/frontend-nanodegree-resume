0e96ee5b9c2067df802e33d70b1d5787a5dfb35b
hooahvixen <rachel@shebora.com>
2015-03-23 18:44:37 -0400

    fix work exp./middle of lesson 2/JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,28 +12,35 @@ var bio = {
   "loc" : "North Carolina"
 }
 
+
+
+
 /*Work*/
 var work = [
   {
     "position" : "Domestic Engineer",
     "employer" : "Unwanted Children",
-    "years" : 7,
-    "city" : "Worldwide"
+    "years" : "2008-present",
+    "city" : "Fort Bragg, NC (previously: Enkenbach, Germany/ Fort Polk, LA/ Spring Hill, FL)",
+    "desc" : "I have been a stay-at-Home mother so that my husband could follow his dream of joining the army.  I control the finances, buying everything, chauffeur, cook, I'm a 'nurse', provide 'therapy', make and uphold house laws (like judge Dredd), and I handle any other odd job my family requires. I'm amazing at it. Now that my husband is getting out of the army, I've been working on educating myself so he can take a step back and I can become the main bread winner."
   },
   {
     "position" : "Pregnant Receptionist",
     "employer" : "Dance Factory",
     "years" : 2008,
-    "city" : "Arlington, VA"
+    "city" : "Arlington, VA",
+    "desc" : "After my husband went to basic, I needed to make some money so I wouldn't be put out on the streets. I became a receptionist and made $10/hour. I handled calls, administrative work, created flyers, set up for conferences, set up for Friday night parties, threw up a lot (morning sickness), handled money, and I was the first person to greet customers before they went to class (I was really good at keeping students)."
   },
   {
-    "position" : ["Intern", "Receptionist", "Dance Instructor", "Executive Assistant"],
+    "position" : "Intern, Receptionist, Dance Instructor, and Executive Assistant",
     "employer" : "Alliance Dance Institute",
     "years" : "2005-2007",
-    "city" : "Alexandria, VA"
+    "city" : "Alexandria, VA",
+    "desc" : "I got this job, originally, as an extracurricular activity.  I started (and basically stayed) as a receptionist. I greeted and scheduled customers; created, printed, and handed out flyers; handled money, bought things and set up for parties. Later on I began training to dance. Instead of recieving money, I traded my time for coaching lessons. I trained for competitions and to recieve my in-house certification for dance. Once I was certified, my job title became executive assistant (to the VP)/Dance instructor. My duties were as follows: teach group and private lessons, provide help for new and old students during parties, clean, set up for parties, type to dictation, edit memos, open the studio, keep students happy, create/update flyers, handle other teachers, set up for conferences, any odd jobs that anyone needed me to do, and continue my dance education. I worked up to 14-hour days and I didn't really have a life beyond the studio and school. Once I met my husband, I realized that I couldn't work 7 days a week anymore. I told this to the president of the studio and he said that he couldn't afford to give me time off, so I quit."
   }
 
 ]
+
 
 /*education*/
 var education = {
@@ -101,3 +108,59 @@ var education = {
 }
 /*Projects*/
 var projects = {};
+
+/*putting it all on the page*/
+var displayWork = function(){
+
+  for (job in work){
+    $("#workExperience").append(HTMLworkStart);
+    var formEmp = HTMLworkEmployer.replace("%data%", work[job].employer);
+    var formPos = HTMLworkTitle.replace("%data%", work[job].position);
+    var formEmpTitle = formEmp + formPos;
+    var formDate = HTMLworkDates.replace("%data%", work[job].years);
+    var formLoc = HTMLworkLocation.replace("%data%", work[job].city);
+    var formDesc = HTMLworkDescription.replace("%data%", work[job].desc);
+    var formInfo = formDate + formLoc + formDesc;
+    var formWork = formEmpTitle + formInfo;
+    $(".work-entry:last").append(formWork);
+  }
+}
+
+var displayBio = function () {
+
+  var formRole = HTMLheaderRole.replace("%data%", bio.role);
+  $("#header").prepend(formRole);
+  var formName = HTMLheaderName.replace("%data%", bio.name);
+  $("#header").prepend(formName);
+  var formImg = HTMLbioPic.replace("%data%", bio.picURL);
+  $("#header").append(formImg);
+
+  var formMobile = HTMLmobile.replace("%data%", bio.phone);
+  var formEmail = HTMLemail.replace("%data%", bio.email);
+  var formGit = HTMLgithub.replace("%data%", bio.git);
+  var formLoc = HTMLlocation.replace("%data%", bio.loc);
+  var formContact = formMobile + formEmail + formGit + formLoc;
+  $("#topContacts").append(formContact);
+
+  if (bio.skills.length > 0) {
+
+    $("#header").append(HTMLskillsStart);
+
+    for (skill in bio.skills){
+      var formSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+      $("#header").append(formSkill);
+    }
+
+  }
+
+}
+
+$(document).click(function (loc) {
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x, y);
+});
+
+
+displayBio();
+displayWork();
